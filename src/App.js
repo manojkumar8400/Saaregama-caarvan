a9ec50cfa8c4a71ed0e33ed6e6024fcfd152006f
Manoj Kumar <70743261+manojkumar8400@users.noreply.github.com>
2021-12-09 11:35:15 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,48 +3,48 @@ import "./styles.css";
 
 var Singers = {
   arijeet_singh: [
-    { movie: "Galti Se Mistake", rating: " rating 4.5/5" },
-    { movie: "Tera Yaar Hoon Main", rating: "rating: 3.8/5" },
-    { movie: "Desh Mere", rating: "rating:4/5" },
-    { movie: "Humdard", rating: "rating: 3/5" }
+    { songs: "Galti Se Mistake", rating: " rating 4.5/5" },
+    { songs: "Tera Yaar Hoon Main", rating: "rating: 3.8/5" },
+    { songs: "Desh Mere", rating: "rating:4/5" },
+    { songs: "Humdard", rating: "rating: 3/5" }
   ],
   sonu_nigam: [
-    { movie: "Bhagwan Hai Kahan Re Tu", rating: "rating: 4.5/5" },
-    { movie: "Bole Chudiyan", rating: "rating: 3.5/5" },
-    { movie: "Papa Mere Papa", rating: "rating: 4/5" },
-    { movie: "Har Ek Friend Kamina Hota Hai", rating: "rating: 4/5" }
+    { songs: "Bhagwan Hai Kahan Re Tu", rating: "rating: 4.5/5" },
+    { songs: "Bole Chudiyan", rating: "rating: 3.5/5" },
+    { songs: "Papa Mere Papa", rating: "rating: 4/5" },
+    { songs: "Har Ek Friend Kamina Hota Hai", rating: "rating: 4/5" }
   ],
   lata_mangeshkar: [
-    { movie: "Aesa Desh Hai Mera", rating: "rating: 5/5" },
-    { movie: "Dil Pardeshi Ho Gya", rating: "rating: 4/5" },
-    { movie: " Tu Kitni Achhi Hai", rating: "rating: 3.5/5" },
-    { movie: "Ho Gya Hai Tujhko To Pyar sajna", rating: "rating: 4/5 " }
+    { songs: "Aesa Desh Hai Mera", rating: "rating: 5/5" },
+    { songs: "Dil Pardeshi Ho Gya", rating: "rating: 4/5" },
+    { songs: " Tu Kitni Achhi Hai", rating: "rating: 3.5/5" },
+    { songs: "Ho Gya Hai Tujhko To Pyar sajna", rating: "rating: 4/5 " }
   ],
   kishor_kumar: [
-    { movie: "Ek Ladki Bheegi Bhaagi Si", rating: " rating: 4/5" },
-    { movie: "Ruk Ja Rokta Hai Yeh Diwana", rating: " rating:3/5" },
-    { movie: "Chhedo Na Meri Zulfen", rating: "rating:4.5/5" },
-    { movie: "Koi Humdard Na Rha", rating: "3.5/5" }
+    { songs: "Ek Ladki Bheegi Bhaagi Si", rating: " rating: 4/5" },
+    { songs: "Ruk Ja Rokta Hai Yeh Diwana", rating: " rating:3/5" },
+    { songs: "Chhedo Na Meri Zulfen", rating: "rating:4.5/5" },
+    { songs: "Koi Humdard Na Rha", rating: "3.5/5" }
   ]
 };
 
-var movieknow = Object.keys(Singers);
+var songsknow = Object.keys(Singers);
 
 export default function App() {
-  const [movie, setmovie] = useState("arijeet_singh");
+  const [songs, setsongs] = useState("arijeet_singh");
   function genreClickHandler(genre) {
-    setmovie(genre);
+    setsongs(genre);
   }
   return (
     <div className="App">
-      <h1>🎥Bollywood-Masala🎥</h1>
+      <h1>Saaregama-Caarvan</h1>
       <p style={{ fontSize: "smaller" }}>
         {" "}
         Checkout my favorite Singers Song. Select a Singer to get started...{" "}
       </p>
 
       <div>
-        {movieknow.map((genre) => (
+        {songsknow.map((genre) => (
           <button
             onClick={() => genreClickHandler(genre)}
             style={{
@@ -63,7 +63,7 @@ export default function App() {
       <hr />
       <div style={{ textAlign: "center" }}>
         <ul style={{ paddingBlock: "0" }}>
-          {Singers[movie].map((bollywood) => (
+          {Singers[songs].map((bollywood) => (
             <li
               key={bollywood}
               style={{
@@ -75,7 +75,7 @@ export default function App() {
               }}
             >
               {" "}
-              <div style={{ fontSize: "larger" }}> {bollywood.movie} </div>
+              <div style={{ fontSize: "larger" }}> {bollywood.songs} </div>
               <div style={{ fontSize: "smallj" }}> {bollywood.rating} </div>
             </li>
           ))}
